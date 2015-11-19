7308d5d04c2d17f0d280eb39c80fb1a9dd7da3f3
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 10:54:01 +0100

    Extend from Ogone.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_AbnAmro_Internetkassa_Integration {
+class Pronamic_WP_Pay_Gateways_AbnAmro_Internetkassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id       = 'abnamro-internetkassa';
 		$this->name     = 'ABN AMRO - Internetkassa';
 		$this->url      = 'https://internetkassa.abnamro.nl/';
 		$this->provider = 'abnamro';
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Gateway';
 	}
 }
