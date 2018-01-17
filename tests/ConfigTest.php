edc233ea0ab99f1786f74b798803431d0f271196
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 11:56:32 +0100

    Happy 2018.

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: ABN AMRO - Internetkassa - Config test
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
--- tests/TestConfigTest.php
+++ tests/TestConfigTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: ABN AMRO - Internetkassa - Test config test
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
